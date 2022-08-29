29c1f2d8c31e51cd730635d2278f7cd55bb3703a
mickeywonders <chikwadomichael@gmail.com>
2022-08-29 03:35:26 -0700

    cpython tasks 3

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 def no_c(my_string):
-    str = ""
-    for i in my_string:
-        if i != "c" and i != "C":
-            str = str + i
-    return str
+    new_string = ""
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_string += c
+    return new_string
