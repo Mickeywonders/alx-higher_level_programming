2b008fefa4c07d411055e4983835c22015e4612b
Nnakwe Michael <chikwadomichael@gmail.com>
2022-09-13 16:41:55 -0700

    okay

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Define classes for a singly-linked list."""
 
 
@@ -40,7 +41,7 @@ class SinglyLinkedList:
     """Represent a singly-linked list."""
 
     def __init__(self):
-        """Initialize a new SinglyLinkedList."""
+        """Initalize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
