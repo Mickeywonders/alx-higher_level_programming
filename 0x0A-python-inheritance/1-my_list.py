eb149711709e6e3c20cbe67c2091f54df4681789
Nnakwe Michael <chikwadomichael@gmail.com>
2022-09-26 20:59:17 -0700

    push

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-contains the MyList class
+Class inheritance
 """
 
 
 class MyList(list):
-    """a subclass of list"""
-    def __init__(self):
-        """initializes the object"""
-        super().__init__()
+    """
+    a class MyList that inherits from list
+    """
 
     def print_sorted(self):
-        """prints the sorted list"""
+        """
+        prints the sorted list
+        """
         print(sorted(self))
